cd4f81d4fb1ac41eb5b2ca318070e70a9ff1dae2
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-04 14:12:10 +0300

    Updates

---
+++ precache-manifest.daf11e13204ae7edbd9451de18c77e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40e24d1628247b30323960f2e10c6e4",
+    "url": "/React-Menu-Project/index.html"
+  },
+  {
+    "revision": "d8d15182263b68ec3b2c",
+    "url": "/React-Menu-Project/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/React-Menu-Project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Menu-Project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d15182263b68ec3b2c",
+    "url": "/React-Menu-Project/static/js/main.b3038075.chunk.js"
+  },
+  {
+    "revision": "9565333620688d66067c",
+    "url": "/React-Menu-Project/static/js/runtime-main.6f266fab.js"
+  }
+]);
